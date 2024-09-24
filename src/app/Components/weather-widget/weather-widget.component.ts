f7abf8d69155ae2b6e6ea3944724a6b801f79526
Yousef <ioyx515@gmail.com>
2024-09-24 22:19:50 +0300

    Create the first section of the app.

--- src/app/Components/weather-widget/weather-widget.component.ts
+++ src/app/Components/weather-widget/weather-widget.component.ts
@@ -1,31 +1,34 @@
 import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { WeatherService } from '../../Core/Services/weather.service';
 import { tap } from 'rxjs/operators';
+import { Weather } from '../../Core/Interfaces/weather';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-weather-widget',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './weather-widget.component.html',
   styleUrl: './weather-widget.component.scss'
 })
 export class WeatherWidgetComponent implements OnInit {
   private readonly _WeatherService = inject(WeatherService);
   userCurrentCity: WritableSignal<string> = signal("");
+  currentWeatherData!: Weather;
 
   getUserCity() {
     return this._WeatherService.getUserCity().pipe(
       tap(res => {
         this.userCurrentCity.set(res.city);
-        console.log(this.userCurrentCity());
-        this.getCurrent();
+        this.getForecast();
       })
     ).subscribe();
   }
 
-  getCurrent() {
-    this._WeatherService.getCurrent(this.userCurrentCity()).subscribe({
+  getForecast() {
+    this._WeatherService.getForecast(this.userCurrentCity(), 7).subscribe({
       next: (res) => {
+        this.currentWeatherData = res;
         console.log(res);
       }
     });
